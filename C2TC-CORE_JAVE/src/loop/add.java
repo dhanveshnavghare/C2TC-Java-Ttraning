c9de8461585334bb12c1a4bab83fb4e52905de44
NAMRATA NAVGHARE <NAMRATA NAVGHARE@DESKTOP-5NJ6TES>
2022-06-09 12:52:08 +0530

    add

---
+++ C2TC-CORE_JAVE/src/loop/add.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+
+	}
+
+}
