45e2120205e5c0a5d941193bfd65351727c54f7e
NAMRATA NAVGHARE <NAMRATA NAVGHARE@DESKTOP-5NJ6TES>
2022-06-15 14:50:59 +0530

    Addition

---
+++ C2TC-CORE_JAVE/src/loop/Addition.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int x = 10, y=20;
+		System.out.println(x+y);   // 30
+		System.out.println(--x);   // 9 
+		System.out.println(++y);   //21
+		
+		System.out.println(--x);   // 8
+		System.out.println(x--);  //8
+		System.out.println(x-- + y++);  //28
+
+
+	}
+
+}
